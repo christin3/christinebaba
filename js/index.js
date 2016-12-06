d0271eea8311c58ece61dff6df82783a1ab9c8f2
Christine Baba <baba.christine@gmail.com>
2016-12-05 23:46:16 -0600

    Under Construction completed

--- js/index.js
+++ js/index.js
@@ -1,16 +1,214 @@
-// particle.min.js hosted on GitHub
-// Scroll down for initialisation code
+/*
+ * requestAnimationFrame pollyfill
+ */
+if (!window.requestAnimationFrame) {
+	window.requestAnimationFrame = (window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame || function (callback) {
+		return window.setTimeout(callback, 1000 / 60);
+	});
+}
 
-!function(a){var b="object"==typeof self&&self.self===self&&self||"object"==typeof global&&global.global===global&&global;"function"==typeof define&&define.amd?define(["exports"],function(c){b.ParticleNetwork=a(b,c)}):"object"==typeof module&&module.exports?module.exports=a(b,{}):b.ParticleNetwork=a(b,{})}(function(a,b){var c=function(a){this.canvas=a.canvas,this.g=a.g,this.particleColor=a.options.particleColor,this.x=Math.random()*this.canvas.width,this.y=Math.random()*this.canvas.height,this.velocity={x:(Math.random()-.5)*a.options.velocity,y:(Math.random()-.5)*a.options.velocity}};return c.prototype.update=function(){(this.x>this.canvas.width+20||this.x<-20)&&(this.velocity.x=-this.velocity.x),(this.y>this.canvas.height+20||this.y<-20)&&(this.velocity.y=-this.velocity.y),this.x+=this.velocity.x,this.y+=this.velocity.y},c.prototype.h=function(){this.g.beginPath(),this.g.fillStyle=this.particleColor,this.g.globalAlpha=.7,this.g.arc(this.x,this.y,1.5,0,2*Math.PI),this.g.fill()},b=function(a,b){this.i=a,this.i.size={width:this.i.offsetWidth,height:this.i.offsetHeight},b=void 0!==b?b:{},this.options={particleColor:void 0!==b.particleColor?b.particleColor:"#fff",background:void 0!==b.background?b.background:"#1a252f",interactive:void 0!==b.interactive?b.interactive:!0,velocity:this.setVelocity(b.speed),density:this.j(b.density)},this.init()},b.prototype.init=function(){if(this.k=document.createElement("div"),this.i.appendChild(this.k),this.l(this.k,{position:"absolute",top:0,left:0,bottom:0,right:0,"z-index":1}),/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.background))this.l(this.k,{background:this.options.background});else{if(!/\.(gif|jpg|jpeg|tiff|png)$/i.test(this.options.background))return console.error("Please specify a valid background image or hexadecimal color"),!1;this.l(this.k,{background:'url("'+this.options.background+'") no-repeat center',"background-size":"cover"})}if(!/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(this.options.particleColor))return console.error("Please specify a valid particleColor hexadecimal color"),!1;this.canvas=document.createElement("canvas"),this.i.appendChild(this.canvas),this.g=this.canvas.getContext("2d"),this.canvas.width=this.i.size.width,this.canvas.height=this.i.size.height,this.l(this.i,{position:"relative"}),this.l(this.canvas,{"z-index":"20",position:"relative"}),window.addEventListener("resize",function(){return this.i.offsetWidth===this.i.size.width&&this.i.offsetHeight===this.i.size.height?!1:(this.canvas.width=this.i.size.width=this.i.offsetWidth,this.canvas.height=this.i.size.height=this.i.offsetHeight,clearTimeout(this.m),void(this.m=setTimeout(function(){this.o=[];for(var a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&this.o.push(this.p),requestAnimationFrame(this.update.bind(this))}.bind(this),500)))}.bind(this)),this.o=[];for(var a=0;a<this.canvas.width*this.canvas.height/this.options.density;a++)this.o.push(new c(this));this.options.interactive&&(this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p),this.canvas.addEventListener("mousemove",function(a){this.p.x=a.clientX-this.canvas.offsetLeft,this.p.y=a.clientY-this.canvas.offsetTop}.bind(this)),this.canvas.addEventListener("mouseup",function(a){this.p.velocity={x:(Math.random()-.5)*this.options.velocity,y:(Math.random()-.5)*this.options.velocity},this.p=new c(this),this.p.velocity={x:0,y:0},this.o.push(this.p)}.bind(this))),requestAnimationFrame(this.update.bind(this))},b.prototype.update=function(){this.g.clearRect(0,0,this.canvas.width,this.canvas.height),this.g.globalAlpha=1;for(var a=0;a<this.o.length;a++){this.o[a].update(),this.o[a].h();for(var b=this.o.length-1;b>a;b--){var c=Math.sqrt(Math.pow(this.o[a].x-this.o[b].x,2)+Math.pow(this.o[a].y-this.o[b].y,2));c>120||(this.g.beginPath(),this.g.strokeStyle=this.options.particleColor,this.g.globalAlpha=(120-c)/120,this.g.lineWidth=.7,this.g.moveTo(this.o[a].x,this.o[a].y),this.g.lineTo(this.o[b].x,this.o[b].y),this.g.stroke())}}0!==this.options.velocity&&requestAnimationFrame(this.update.bind(this))},b.prototype.setVelocity=function(a){return"fast"===a?1:"slow"===a?.33:"none"===a?0:.66},b.prototype.j=function(a){return"high"===a?5e3:"low"===a?2e4:isNaN(parseInt(a,10))?1e4:a},b.prototype.l=function(a,b){for(var c in b)a.style[c]=b[c]},b});
+// Init Stats
+var stats = new Stats();
+//stats.setMode(0);
+//stats.domElement.style.position = 'absolute';
+//stats.domElement.style.left = '0px';
+//stats.domElement.style.top = '0px';
+//document.body.appendChild(stats.domElement);
 
-// Initialisation
 
-var canvasDiv = document.getElementById('particle-canvas');
-var options = {
-  particleColor: '#6b61b7',
-  background: 'http://i.imgur.com/6n9IFEe.jpg',
-  interactive: true,
-  speed: 'high',
-  density: 'high'
-};
-var particleCanvas = new ParticleNetwork(canvasDiv, options);
+/*!
+ * Mantis.js / jQuery / Zepto.js plugin for Constellation
+ * @version 1.2.2
+ * @author Acauã Montiel <contato@acauamontiel.com.br>
+ * @license http://acaua.mit-license.org/
+ */
+(function ($, window) {
+	/**
+	 * Makes a nice constellation on canvas
+	 * @constructor Constellation
+	 */
+	function Constellation (canvas, options) {
+		var $canvas = $(canvas),
+			context = canvas.getContext('2d'),
+			defaults = {
+				star: {
+					color: 'rgb(125, 66, 244)',
+					width: 1,
+					randomWidth: true
+				},
+				line: {
+					color: 'rgb(125, 66, 244)',
+					width: 0.4
+				},
+				position: {
+					x: 0, // This value will be overwritten at startup
+					y: 0 // This value will be overwritten at startup
+				},
+				width: window.innerWidth,
+				height: window.innerHeight,
+				velocity: 0.2,
+				length: 200,
+				distance: 120,
+				radius: 500,
+				stars: []
+			},
+			config = $.extend(true, {}, defaults, options);
+
+		function Star () {
+			this.x = Math.random() * canvas.width;
+			this.y = Math.random() * canvas.height;
+
+			this.vx = (config.velocity - (Math.random() * 0.5));
+			this.vy = (config.velocity - (Math.random() * 0.5));
+
+			this.radius = config.star.randomWidth ? (Math.random() * config.star.width) : config.star.width;
+		}
+
+		Star.prototype = {
+			create: function(){
+				context.beginPath();
+				context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+				context.fill();
+			},
+
+			animate: function(){
+				var i;
+				for (i = 0; i < config.length; i++) {
+
+					var star = config.stars[i];
+
+					if (star.y < 0 || star.y > canvas.height) {
+						star.vx = star.vx;
+						star.vy = - star.vy;
+					} else if (star.x < 0 || star.x > canvas.width) {
+						star.vx = - star.vx;
+						star.vy = star.vy;
+					}
+
+					star.x += star.vx;
+					star.y += star.vy;
+				}
+			},
+
+			line: function(){
+				var length = config.length,
+					iStar,
+					jStar,
+					i,
+					j;
+
+				for (i = 0; i < length; i++) {
+					for (j = 0; j < length; j++) {
+						iStar = config.stars[i];
+						jStar = config.stars[j];
+
+						if (
+							(iStar.x - jStar.x) < config.distance &&
+							(iStar.y - jStar.y) < config.distance &&
+							(iStar.x - jStar.x) > - config.distance &&
+							(iStar.y - jStar.y) > - config.distance
+						) {
+							if (
+								(iStar.x - config.position.x) < config.radius &&
+								(iStar.y - config.position.y) < config.radius &&
+								(iStar.x - config.position.x) > - config.radius &&
+								(iStar.y - config.position.y) > - config.radius
+							) {
+								context.beginPath();
+								context.moveTo(iStar.x, iStar.y);
+								context.lineTo(jStar.x, jStar.y);
+								context.stroke();
+								context.closePath();
+							}
+						}
+					}
+				}
+			}
+		};
+
+		this.createStars = function () {
+			var length = config.length,
+				star,
+				i;
+
+			context.clearRect(0, 0, canvas.width, canvas.height);
+
+			for (i = 0; i < length; i++) {
+				config.stars.push(new Star());
+				star = config.stars[i];
+
+				star.create();
+			}
+
+			star.line();
+			star.animate();
+		};
+
+		this.setCanvas = function () {
+			canvas.width = config.width;
+			canvas.height = config.height;
+		};
+
+		this.setContext = function () {
+			context.fillStyle = config.star.color;
+			context.strokeStyle = config.line.color;
+			context.lineWidth = config.line.width;
+		};
+
+		this.setInitialPosition = function () {
+			if (!options || !options.hasOwnProperty('position')) {
+				config.position = {
+					x: canvas.width * 0.5,
+					y: canvas.height * 0.5
+				};
+			}
+		};
+
+		this.loop = function (callback) {
+			callback();
+
+			window.requestAnimationFrame(function () {
+				stats.begin(); // Only for Stats
+				this.loop(callback);
+				stats.end(); // Only for Stats
+			}.bind(this));
+		};
+
+		this.bind = function () {
+			$canvas.on('mousemove', function(e){
+				config.position.x = e.pageX - $canvas.offset().left;
+				config.position.y = e.pageY - $canvas.offset().top;
+			});
+		};
+
+		this.init = function () {
+			this.setCanvas();
+			this.setContext();
+			this.setInitialPosition();
+			this.loop(this.createStars);
+			this.bind();
+		};
+	}
+
+	$.fn.constellation = function (options) {
+		return this.each(function () {
+			var c = new Constellation(this, options);
+			c.init();
+		});
+	};
+})($, window);
+
+// Init plugin
+$('canvas').constellation({
+	star: {
+		width:5
+	},
+	line: {
+		color: 'rgb(125, 66, 244)'
+	},
+	radius: 100
+
+
+
+
+});

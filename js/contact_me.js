72cf8037bfed01225836b0b2e933e5e23aa4c236
Christine <baba.christine@gmail.com>
2017-01-20 13:52:36 -0600

    contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "http://www.christinebaba.com/mail/contact_me.php",
+                url: "mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,

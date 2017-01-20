910994322a92587004426560a851c32993f6d93f
Christine <baba.christine@gmail.com>
2017-01-19 21:32:27 -0600

    contact

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -21,7 +21,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'baba.christine@gmail.com';
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@enom.com\n";
+$headers = "From: noreply@github.com\n";
 $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 $response = array();

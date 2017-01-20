e28e2768717219be2e024b32e544ed975fcb49ff
Christine <baba.christine@gmail.com>
2017-01-20 14:04:25 -0600

    CONTACTME
    WHY WONT YOU WORK?! SKDFJL:S

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://www.apparelart.com/Bootcamp/contact_me.php",
+                url: "https://www.christinebaba.com/Scripts/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,

12d9a303d11499e3203d1fd033a854abb607c3cf
Christine <baba.christine@gmail.com>
2017-01-20 14:01:26 -0600

    contact

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,8 +21,8 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "mail/contact_me.php",
-                type: "PUT",
+                url: "https://www.apparelart.com/Bootcamp/contact_me.php",
+                type: "POST",
                 data: {
                     name: name,
                     phone: phone,
